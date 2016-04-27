7c3ef54b488fa50e0a192b54958d73a0265f9e4d
Docmudkipz <tehdocmudkipz@gmail.com>
2016-04-26 22:53:34 -0700

    Added support for extending static images
    and fps lower than 5

--- makeanim.sh
+++ makeanim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-minfps=5
+minfps=1
 maxfps=30
 echo "What is the name of the file you want to convert(with extension)?"
 read input
@@ -7,7 +7,7 @@ read input
 echo "What do you want your framerate to be, any number between $minfps and $maxfps?"
 read fps
 	if [ $fps -lt $minfps ]; then
-		echo "Please pick a number greater than 5"
+		echo "Please pick a number greater than 1"
 		read -p "Press enter to continue"
 		sh makeanim.sh
 	elif [ $fps -gt $maxfps ]; then
@@ -22,18 +22,37 @@ echo "0 for top screen and 1 for bottom screen"
 read number
 	if [ $number = 0 ]; then
 		size="400:240"
+		magic=""400x240""
 		rename="anim"
 		rm "anim"
 	else
 		size="320:240"
+		magic=""320x240""
 		rename="bottom_anim"
 		rm "bottom_anim"
 fi
-ffmpeg -y -i $input -vf fps=$fps,scale=$size:flags=lanczos,transpose=1 -pix_fmt bgr24 output.rgb
-mv output.rgb $rename
 
-rm config
-cp configs/$fps ./
-mv $fps config
+echo "0 for static image, 1 for animation"
+read img
+	if [ $img = 0 ]; then
+		echo "How long in seconds do you want the image to last?"
+		read second
+		let frames=$[fps*second]
+		convert $input -resize $magic -flatten -channel BGR -separate -channel RGB -combine -rotate 90 $rename.rgb
+		for i in $(seq 1 $frames); do cat $rename.rgb >> $rename; done
+		rm $rename.rgb
+		rm config
+		cp ./configs/$fps ./
+		mv $fps config
+		exit
 
-exit
+	else	
+
+		ffmpeg -y -i $input -vf fps=$fps,scale=$size:flags=lanczos,transpose=1 -pix_fmt bgr24 output.rgb
+		mv output.rgb $rename
+		rm config
+		cp ./configs/$fps ./
+		mv $fps config
+		exit
+
+fi
