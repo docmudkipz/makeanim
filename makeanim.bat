4c5c50a9caa00800686b68a70c5ffb7f376592be
Wolfvak <soherrera1@hotmail.com>
2016-05-22 17:18:03 -0300

    Fixed Windows script
    Previously it used the (input size * 0.4) as a threshold. Fixed to use
    (animation size * 0.5)
    
    Still need to fix *nix shell script

--- makeanim.bat
+++ makeanim.bat
@@ -1,29 +1,35 @@
 @ECHO OFF
 set minfps=1
 set maxfps=30
+set version=0.1
+
+del output.bin
+
+cls
+echo makeanim v%version% by Docmudkipz
 
 IF [%1]==[] (
-	set /p input="what is the name with extension of the file you want to convert?(ex. hi.gif)"
+	set /p input="What is the file you want to convert? (ex. hi.gif) "
 ) else (
 	set input=%1
 )
 
-FOR /F "usebackq" %%A IN ('%input%') DO set threshold=%%~zA 
-FOR /F "delims=" %%G in ('powershell %threshold%*0.4') DO set threshold=%%G
-cls
-del output.bin
-
 :fps
 	cls
-	set /p fps="What do you want your framerate to be, any number between %minfps% and %maxfps%: "
+	set /p fps="Please input your framerate, between %minfps% and %maxfps%: "
+
 		if /I %fps% LSS %minfps% (
-			echo "Please pick a number greater than 5"
+			echo Please pick a number greater or equal than %minfps%!
 			Pause >nul
-			GOTO fps  )
-		if /I %fps% GTR %maxfps%	(
-			echo "Please pick a number lower than 30"
+			GOTO fps
+		)
+
+		if /I %fps% GTR %maxfps% (
+			echo Please pick a number lower or equal than %maxfps%!
 			Pause >nul
-			GOTO fps )
+			GOTO fps
+		)
+
 		for /f "delims=" %%i in ('powershell -Command "'{0:x}' -f %fps%"') do set hex=%%i
 		set hex=0x%hex%
 		echo %hex%
@@ -40,7 +46,7 @@ del output.bin
 	set comp=0x00
 	del anim
 	GOTO img
-	
+
 :bot
 	set res=320:240
 	set magick="320x240"
@@ -52,54 +58,74 @@ del output.bin
 
 :img
 	cls
-	CHOICE /M "Is this a static image? A for yes B for no: " /C:ab
-	IF ERRORLEVEL 2 GOTO ff
-	IF ERRORLEVEL 1 GOTO static
-	
+	CHOICE /M "Is this an animation? " /C:yn
+	IF ERRORLEVEL 1 GOTO ff
+	IF ERRORLEVEL 2 GOTO static
+
 :static
 	cls
-	set /p time="How long in seconds do you want the image to last?: "
+	set /p time="How many seconds do you want the image to last?: "
 	set /a "frame=%time%*%fps%"
 	del %name%.rgb
 
 	convert.exe %input% -flatten -resize %magick% -channel BGR -separate -channel RGB -combine -rotate 90 %name%.rgb
-	
+
+	if ERRORLEVEL 1 (
+		echo ERROR! convert.exe (ImageMagick) couldn't be found! Aborting...
+		goto end
+	)
+
 	for /l %%x in (1, 1, %frame%) do copy /b %name%.rgb %name%.frame.%%x
 		copy /b %name%.frame.* output.bin
 		del *.frame.*
 		del %name%.rgb
 	GOTO compress
-	
+
 :ff
 	cls
+	REM ffmpeg only outputs correctly when the extension is .rgb so...
 	ffmpeg -y -i %input% -vf fps=%fps%,scale=%res%:flags=lanczos,transpose=1 -pix_fmt bgr24 output.rgb
-	rename output.rgb output.bin
+	rename output.rgb %name%
 	goto compress
 
 :compress
-	CHOICE /M "Do you want this to be compressed using ba9comp? A for yes B for no: " /C:ab
+	CHOICE /M "Do you want this to be compressed with ban9comp? " /C:yn
 	IF ERRORLEVEL 2 GOTO end
 	IF ERRORLEVEL 1 GOTO comp
-	
+
 :comp
-	ban9comp.exe c %s% < output.bin > compressed.bin
-	rename compressed.bin %name%
-	set comp=0x01
-	del output.bin
-	FOR /F "usebackq" %%A IN ('%name%') DO set compsize=%%~zA
-	if /I %compsize% GTR %threshold% (
-		del %name%
-		echo "This file after compression is too large, it will be decompressed for you"
-		pause
-		rename %name% bfore.bin
-		ba9comp.exe d %s% < bfore.bin > output.bin
-		rename output.bin %name%
+	ban9comp.exe c %s% < %name% > compressed_%name%
+
+	if ERRORLEVEL 1 (
+		echo WARNING! ban9comp.exe couldn't be executed! Make sure it's in PATH!
+		del compressed_%name%
 		set comp=0x00
-		GOTO end )
-	
+		goto end
+	)
+	REM in casy ban9comp.exe can't be launched, bail out
+
+	set comp=0x01
+	FOR /F "usebackq" %%A IN ('compressed_%name%') DO set compsize=%%~zA
+
+	FOR /F "usebackq" %%A IN ('%name%') DO set threshold=%%~zA 
+	FOR /F "delims=" %%G in ('powershell %threshold%*0.5') DO set threshold=%%G
+	REM set threshold size
+
+	if /I %compsize% GTR %threshold% (
+		echo This file is too large after compression, so it will be deleted. Try reducing the framerate.
+		pause
+		del compressed_%name%
+		REM compressed animation is too large to provide a tolerable experience, delete
+		set comp=0x00
+		GOTO end
+	) else (
+		del %name%
+		ren compressed_%name% %name%
+		REM delete uncompressed animation and rename the compressed one
+	)
 	goto end
+
 :end
-	copy NUL EmptyFile.txt
-	rename EmptyFile.txt config
+	copy NUL config
 	powershell -Command "[Byte[]] $x = %hex%,%comp%; set-content -value $x -encoding byte -path config"
-	exit
+	pause
